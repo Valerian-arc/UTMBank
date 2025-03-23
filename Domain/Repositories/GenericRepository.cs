2d22add25cf6a82b20d0588b947ad805a0e37b68
PaulaFortuna909 <paulafortuna111@gmail.com>
2025-03-23 20:21:23 +0200

    Repozitorii si enumuri

---
+++ Domain/Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using Domain.Data.Context;
+using Domain.Entites;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Domain.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly BankDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(BankDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var entity = _dbSet.Find(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Domain/Repositories/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entites;
+using System.Collections.Generic;
+
+namespace Domain.Repositories
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        void Add(T entity);
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        void Update(T entity);
+        void Delete(int id);
+    }
+}
