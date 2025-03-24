2d35b13c9ca99464d0ec8caee6b5e083e74c21c9
Gutu Anastasia <anastasia.gutu@isa.utm.md>
2025-03-24 19:21:15 +0200

    Modificare si adaugarea pentru noi View

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using AutoMapper;
+using Core.Services.UserServices;
+using Domain.Data.Context;
+using Domain.Repositories;
+using Helpers.Profiles;
+using System;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
+using Web.Controllers;
 
 namespace Web
 {
@@ -13,9 +17,53 @@ namespace Web
     {
         void Application_Start(object sender, EventArgs e)
         {
-            // Code that runs on application startup
-           AreaRegistration.RegisterAllAreas();
-           RouteConfig.RegisterRoutes(RouteTable.Routes);
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            // Initialize Autofac container
+            var containerBuilder = new ContainerBuilder();
+
+            // Register types and services
+            RegisterTypes(containerBuilder);
+
+            // Build the container
+            var container = containerBuilder.Build();
+
+            // Set Autofac as the Dependency Resolver for MVC
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+        private static void RegisterTypes(ContainerBuilder builder)
+        {
+            // Register DbContext
+            builder.RegisterType<BankDbContext>()
+                   .AsSelf()
+                   .InstancePerRequest();
+
+            // Register generic repository
+            builder.RegisterGeneric(typeof(GenericRepository<>))
+                   .As(typeof(IGenericRepository<>))
+                   .InstancePerRequest();
+
+            // Register UserService
+            builder.RegisterType<UserService>()
+                   .As<IUserService>()
+                   .InstancePerRequest();
+
+            // Register AutoMapper
+            builder.Register(c =>
+            {
+                var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile(new UserProfile());
+                });
+
+                var mapper = config.CreateMapper();
+                return mapper;
+            }).As<IMapper>().InstancePerRequest();
+
+            // Register UserController explicitly
+            builder.RegisterType<UserController>().InstancePerRequest();
         }
     }
 }
