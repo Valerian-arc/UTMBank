df2c25b21d612ac6f86c68ae9db5c01b9383ef8b
Gutu Anastasia <anastasia.gutu@isa.utm.md>
2025-04-13 20:11:11 +0300

    Entitati update nou

---
+++ Domain/Entites/Session.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entites
+{
+    public class Session : BaseEntity
+    {
+        [Required]
+        [StringLength(30)]
+        public string Email { get; set; }
+
+        [Required]
+        public string CookieString { get; set; }
+
+        [Required]
+        public DateTime ExpireTime { get; set; }
+    }
+}
