f786c99c5378e30ab311dd2fc65ebcda81fdfc73
Gutu Anastasia <anastasia.gutu@isa.utm.md>
2025-05-17 16:11:01 +0300

    Entitati noi

---
+++ Domain/Entites/Card.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entites
+{
+    public class Card : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ Domain/Entites/HistoryLog.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entites
+{
+    public class HistoryLog : BaseEntity
+    {
+        public string Source { get; set; }
+        public string Destination { get; set; }
+        public double Amount { get; set; }
+        public string Type { get; set; }
+        public int SourceUser { get; set; }
+        public int DestinationUser { get; set; }
+    }
+}
