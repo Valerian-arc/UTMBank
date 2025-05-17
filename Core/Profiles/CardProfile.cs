26fedef8317c8e7c669b4d5181f16a4808581f50
Valerian-arc <amunlamborghini@gmail.com>
2025-05-17 10:51:19 +0300

    Create Card Mappings

---
+++ Core/Profiles/CardProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Core.DTOs.CardDTOs;
+using Core.Models.CardModels;
+using Domain.Entites;
+
+namespace Core.Profiles
+{
+    public class CardProfile : Profile
+    {
+        public CardProfile() 
+        {
+            CreateMap<Card, CardCreateDTO>().ReverseMap();
+            CreateMap<Card, CardUpdateDTO>().ReverseMap();
+            CreateMap<UpsertCardModel, Card>().ReverseMap();
+            CreateMap<CardCreateDTO, UpsertCardModel>().ReverseMap();
+            CreateMap<CardUpdateDTO, CardUpdateDTO>().ReverseMap();
+            CreateMap<CardUpdateDTO,  UpsertCardModel>().ReverseMap();
+        }
+    }
+}
--- Core/Profiles/UserProfile.cs
+++ Core/Profiles/UserProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Core.DTOs;
+using Core.DTOs.Admin;
 using Core.Models;
 using Domain.Entites;
 using Web.Models;
@@ -14,6 +15,13 @@ namespace Helpers.Profiles
             CreateMap<UserRegisterDTO, UserRegisterViewModel>().ReverseMap();
             CreateMap<User, UserLogInDTO>().ReverseMap();
             CreateMap<UserLogInDTO, UserLogInViewModel>().ReverseMap();
+            CreateMap<UserRegisterDTO, User>().ReverseMap();
+            CreateMap<UserEditDTO, User>()
+                .ForMember(dest => dest.userRoles, opt => opt.MapFrom(src => src.Role));
+            CreateMap<User, UserEditDTO>()
+                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.userRoles));
+            CreateMap<UserCreateDTO, User>().ReverseMap();
+            CreateMap<UserCreateDTO, UserRegisterDTO>().ReverseMap();
         }
     }
 }
